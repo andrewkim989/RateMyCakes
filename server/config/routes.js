53cdba96d200dd9a2714131a4ba1c59f2f60ce38
andrewkim989 <andrewkim989@gmail.com>
2018-09-20 20:54:56 -0700

    first commit

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,4 +1,5 @@
 var cakes = require("../controllers/cakes.js");
+var path = require("path");
 
 module.exports = function(app) {
     app.get("/cakes", function(req, res) {
@@ -15,5 +16,9 @@ module.exports = function(app) {
     
     app.post("/cakes/:id", function(req, res) {
         cakes.review(req, res);
+    });
+
+    app.all("*", (req, res, next) => {
+        res.sendFile(path.resolve("./RateMyCakes/dist/RateMyCakes/index.html"));
     });
 }
